e4dadd56cf6e7c6e57c4ec179ed094cd0c84f92e
Owen Davis <owendavis1991@gmail.com>
2024-09-19 21:08:55 +0000

    "forgot to add my api and secret api keys to cloudinary url"

--- graphicsblog/settings.py
+++ graphicsblog/settings.py
@@ -12,20 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
-cloudinary.config(
-  cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
-  api_key = os.environ.get('CLOUDINARY_API_KEY'),
-  api_secret = os.environ.get('CLOUDINARY_API_SECRET')
-)
-
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
