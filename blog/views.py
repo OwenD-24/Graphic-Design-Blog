ad8a1bf03205f35ff88baf0dbbca733e0d75938d
Owen Davis <owendavis1991@gmail.com>
2024-09-20 13:06:53 +0000

    Added for and if loops to display titles and dummy content

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+posts = [
+    {
+        'author': 'Owen Davis',
+        'title': 'Blog Post 1',
+        'content': 'This is my first blog post',
+        'date_posted': '8th February, 2024',
+    },
+    {
+        'author': 'Owen D',
+        'title': 'Blog Post 2',
+        'content': 'This is my second blog post',
+        'date_posted': '13th February, 2024',
+    }
+]
+
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': "About Page"})
