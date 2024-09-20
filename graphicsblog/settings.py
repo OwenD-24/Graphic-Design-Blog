ec6ae124cef5b258c53bf3e4fd6f6d5bd3a6a08a
Owen Davis <owendavis1991@gmail.com>
2024-09-20 09:25:29 +0000

    Updated staticfiles in settings.py

--- graphicsblog/settings.py
+++ graphicsblog/settings.py
@@ -140,7 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 STATIC_URL = '/static/'
 
 # Default primary key field type
