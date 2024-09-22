adee9a5db41252da70d14a9281aaa552bbb84190
Owen Davis <owendavis1991@gmail.com>
2024-09-22 13:04:44 +0000

    Added data model for posts and also implemented the  Post.object.all() in views.py to get rid of test post templates

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-
-posts = [
-    {
-        'author': 'Owen Davis',
-        'title': 'Blog Post 1',
-        'content': 'This is my first blog post',
-        'date_posted': '8th February, 2024',
-    },
-    {
-        'author': 'Owen D',
-        'title': 'Blog Post 2',
-        'content': 'This is my second blog post',
-        'date_posted': '13th February, 2024',
-    }
-]
-
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
