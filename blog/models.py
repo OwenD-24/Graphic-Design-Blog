7d82bb41612761556700f966b346b397af279dce
Owen Davis <owendavis1991@gmail.com>
2024-09-22 14:12:37 +0000

    Trying to view the content and author of posts, added new urls path to go with new views.py def and created a new post_detail.html files as I had NoReverseMatch at / error, bug fix should now be fixed

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    excerpt = models.TextField(blank=True) 
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .models import Post
 
 
 def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
 
 def about(request):
     return render(request, 'blog/about.html', {'title': "About Page"})
+
+    def post_detail(request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        return render(request, 'blog/post_detail.html', {'post': post})
