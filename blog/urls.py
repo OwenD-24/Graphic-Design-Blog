41fa9490566c57164d60127083330e7c1ecf534e
Owen Davis <owendavis1991@gmail.com>
2024-09-23 12:39:00 +0000

    added post confirm_dlete.html and post_detail and form.html file structures and linked them to views.py and urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('post/<int:post_id>/', views.post_detail, name="post_detail"),
+    # path('', views.home, name="blog-home"),
+    path('about/', views.about, name="blog-about"),
+
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post-new/', PostCreateView.as_view(), name="blog-new"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="blog-update"),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name="blog-delete")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,59 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-def home(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
+# def home(request):
+    # posts = Post.objects.all()
+    # return render(request, 'blog/home.html', {'posts': posts})
 
 def about(request):
     return render(request, 'blog/about.html', {'title': "About Page"})
 
-def post_detail(request, post_id):
-        post = get_object_or_404(Post, id=post_id)
-        return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
