1cf486d50732ddd879a82fd0accbf20727257ec6
Owen Davis <owendavis1991@gmail.com>
2024-09-24 11:03:26 +0000

    add: profile, profile_update and register.html files

--- graphicsblog/settings.py
+++ graphicsblog/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -29,11 +28,6 @@ if os.path.isfile("env.py"):
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# Database Configuration
-DATABASES = {
-    'default': dj_database_url.parse(str(os.environ.get("DATABASE_URL")))
-}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -56,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'users',
+    'crispy_forms',
     'cloudinary_storage',
     'cloudinary',
 ]
@@ -105,6 +100,11 @@ WSGI_APPLICATION = 'graphicsblog.wsgi.application'
     #}
 #}
 
+# Database Configuration
+DATABASES = {
+    'default': dj_database_url.parse(str(os.environ.get("DATABASE_URL")))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -134,6 +134,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
@@ -152,3 +154,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
