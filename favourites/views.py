1f76d944529b570a1b59dcf8f6ca03d008aad0f5
Owen Davis <owendavis1991@gmail.com>
2024-09-30 16:38:56 +0000

    ADD: Added the favourites views.py using @login_required making sure you can only see your favourites only and only when logged in

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Favorite
 
-# Create your views here.
+@login_required
+def favorites_list(request):
+    favorites = Favorite.objects.filter(user=request.user)
+    context = {
+        'favorites': favorites
+    }
+    return render(request, 'favorites/favorites_list.html', context)
+
