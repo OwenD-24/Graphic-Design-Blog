a9e909216f6e2c7260103caa7031aef5c3d6be2f
Owen Davis <owendavis1991@gmail.com>
2024-09-30 16:47:30 +0000

    UPDATE: Forgot to save files of updated URL.py file

--- graphicsblog/urls.py
+++ graphicsblog/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('profile/profile_update/', user_views.profile_update, name="profile-update"),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
-    path('favorites/', include('favorites.urls')),
+    path('favourites/', include('favourites.urls')),
 ]
 
 
