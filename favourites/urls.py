759b0f9ff4328d34703ccad431d427f43e7d61c8
Owen Davis <owendavis1991@gmail.com>
2024-10-01 09:32:48 +0000

    BUG: Updated favourites/views.py as my return direct line was directed to post-detail instead of blog-detail

--- favourites/urls.py
+++ favourites/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.favourites_list, name='favourites-list'),
+    path('favourite/<int:post_id>/', views.favourite_post, name='favourite-post'),
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from blog.models import Post
 from .models import Favourite
 
 @login_required
-def favourites_list(request):
-    favourites = Favourite.objects.filter(user=request.user)
-    context = {
-        'favourites': favourites
-    }
-    return render(request, 'favourites/favourites_list.html', context)
+def favourite_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    favourite, created = Favourite.objects.get_or_create(user=request.user, post=post)
+    
+    if not created:
+        favourite.delete()  
+    
+    return redirect('blog-detail', pk=post_id) 
+
 
