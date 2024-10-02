56a390b64b7be973030dd3757f43a500ff980d01
Owen Davis <owendavis1991@gmail.com>
2024-10-02 11:50:07 +0000

    ADD: Added footer links and made sure they are working so now you can navigate round from the bottom of the page as well as the top, adding social links later

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import (PostCreateView, PostDeleteView, PostDetailView,
                     PostListView, PostUpdateView)
 
 urlpatterns = [
     path('about/', views.about, name="blog-about"),
+    path('users/', include('users.urls')),
     path('', PostListView.as_view(), name="blog-home"),
     path('post-new/', PostCreateView.as_view(), name="blog-new"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
