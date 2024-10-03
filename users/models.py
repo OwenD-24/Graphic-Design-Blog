d61868ad96346ab3282490488a1f9b7120924fe4
Owen Davis <owendavis1991@gmail.com>
2024-10-03 20:59:28 +0000

    AADED: Added soical icons to footer and tried to get them on profile page but did not work as I wanted but I have tried everything

--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) 
     image = models.ImageField(default='default_lahajp.webp', upload_to='profile_pics')
+    github = models.URLField(max_length=200, blank=True, null=True)
+    twitter = models.URLField(max_length=200, blank=True, null=True)
+    linkedin = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
