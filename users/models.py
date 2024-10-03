8188f7eefdbdc452860282d028ad91922e346f0e
Owen Davis <owendavis1991@gmail.com>
2024-10-03 12:26:29 +0000

    UPDATE: Updated static/image folder so new users have a deafult profile picture

--- users/models.py
+++ users/models.py
@@ -12,3 +12,11 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
+
+
+        # resize image code
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
-
 from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm
-
 
 def register(request):
     if request.method == "POST":
@@ -22,15 +20,13 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
 @login_required
 def profile(request, username):
     user = get_object_or_404(User, username=username)
     return render(request, 'users/profile.html', {'user': user})
 
-
 @login_required
-def profile_update(request, username):  
+def profile_update(request, username):
     user = get_object_or_404(User, username=username)
 
     if request.method == 'POST':
@@ -40,14 +36,13 @@ def profile_update(request, username):
         if u_form.is_valid() and p_form.is_valid():
             try:
                 u_form.save()
-                p_form.save()  
+                p_form.save()
                 messages.success(request, 'Your account has been updated!')
-                return redirect('user-profile', username=user.username)  
+                return redirect('user-profile', username=user.username)
             except Exception as e:
                 messages.error(request, f'Error saving profile: {str(e)}')
         else:
             messages.error(request, 'There were errors in the form.')
-
     else:
         u_form = UserUpdateForm(instance=user)
         p_form = ProfileUpdateForm(instance=user.profile)
@@ -57,3 +52,4 @@ def profile_update(request, username):
         "p_form": p_form
     }
     return render(request, 'users/profile_update.html', context)
+
