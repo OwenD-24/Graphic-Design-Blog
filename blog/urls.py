046331dd95356a48c366472b71bc7902599deb6c
Owen Davis <owendavis1991@gmail.com>
2024-10-04 10:21:06 +0000

    TEST: Tested all blog.html files in validator and made sure code was structured properly

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from .views import (PostCreateView, PostDeleteView, PostDetailView,
 urlpatterns = [
     path('about/', views.about, name="blog-about"),
     path('', PostListView.as_view(), name="blog-home"), 
-    path('post-new/', PostCreateView.as_view(), name="blog-new"),
+    path('post-new/', PostCreateView.as_view(), name="blog-post-form"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="blog-update"), 
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="blog-delete"),
